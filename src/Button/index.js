d50ebf7f9cdf1e46827734676007ae3572d6b44d
Varya Stepanova <mail@varya.me>
2017-11-15 21:55:48 +0200

    Remove unused code

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import cx from 'classnames'
-import * as s from './styles'
 
 import ButtonSimple from './ButtonSimple';
 import ButtonLink from './ButtonLink';
 import ButtonLabel from './ButtonLabel';
 
 const Component = ({ href, label, children, ...props }) => {
-  let className = cx('button', props.className)
 
   if (href) {
     return <ButtonLink href={href} {...props}>{children}</ButtonLink>;
